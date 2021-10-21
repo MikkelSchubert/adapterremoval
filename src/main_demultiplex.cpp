f9671d2353308a089073b77dff9268b556e72f56
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2021-10-21 23:42:29 +0200

    output FASTQ statistics when only demultiplexing

--- src/main_demultiplex.cpp
+++ src/main_demultiplex.cpp
@@ -34,6 +34,7 @@
 #include "fastq_io.hpp"
 #include "main.hpp"
 #include "reports.hpp"
+#include "trimming.hpp"
 #include "userconfig.hpp"
 
 //! Implemented in main_adapter_rm.cpp
@@ -45,18 +46,22 @@ add_write_step(const userconfig& config,
                analytical_step* step);
 
 bool
-demux_write_report(const userconfig& config, const ar_statistics& stats)
+demux_write_report(const userconfig& config,
+                   const std::vector<reads_processor*>& processors,
+                   ar_statistics& stats)
 {
+  for (auto ptr : processors) {
+    stats.trimming.push_back(*ptr->get_final_statistics());
+  }
+
   return write_report(config, stats);
 }
 
-class se_demultiplexed_reads_processor : public analytical_step
+class se_demultiplexed_reads_processor : public reads_processor
 {
 public:
   se_demultiplexed_reads_processor(const userconfig& config, size_t nth)
-    : analytical_step(analytical_step::ordering::unordered)
-    , m_config(config)
-    , m_nth(nth)
+    : reads_processor(config, nth)
   {}
 
   chunk_vec process(analytical_chunk* chunk)
@@ -64,9 +69,11 @@ public:
     const size_t offset = m_nth * ai_analyses_offset;
     read_chunk_ptr read_chunk(dynamic_cast<fastq_read_chunk*>(chunk));
 
+    statistics_ptr stats = m_stats.get_sink();
     output_chunk_ptr encoded_reads(new fastq_output_chunk(read_chunk->eof));
 
     for (const auto& read : read_chunk->reads_1) {
+      stats->read_1.process(read);
       encoded_reads->add(*m_config.quality_output_fmt, read);
     }
 
@@ -74,21 +81,17 @@ public:
     chunks.push_back(
       chunk_pair(offset + ai_write_mate_1, std::move(encoded_reads)));
 
+    m_stats.return_sink(std::move(stats));
+
     return chunks;
   }
-
-private:
-  const userconfig& m_config;
-  const size_t m_nth;
 };
 
-class pe_demultiplexed_reads_processor : public analytical_step
+class pe_demultiplexed_reads_processor : public reads_processor
 {
 public:
   pe_demultiplexed_reads_processor(const userconfig& config, size_t nth)
-    : analytical_step(analytical_step::ordering::unordered)
-    , m_config(config)
-    , m_nth(nth)
+    : reads_processor(config, nth)
   {}
 
   chunk_vec process(analytical_chunk* chunk)
@@ -97,6 +100,7 @@ public:
     read_chunk_ptr read_chunk(dynamic_cast<fastq_read_chunk*>(chunk));
     AR_DEBUG_ASSERT(read_chunk->reads_1.size() == read_chunk->reads_2.size());
 
+    statistics_ptr stats = m_stats.get_sink();
     output_chunk_ptr encoded_reads_1(new fastq_output_chunk(read_chunk->eof));
     output_chunk_ptr encoded_reads_2;
     if (!m_config.interleaved_output) {
@@ -108,6 +112,9 @@ public:
     while (it_1 != read_chunk->reads_1.end()) {
       fastq read_1 = *it_1++;
       fastq read_2 = *it_2++;
+
+      stats->read_1.process(read_1);
+      stats->read_2.process(read_2);
 
       encoded_reads_1->add(*m_config.quality_output_fmt, read_1);
 
@@ -126,12 +133,10 @@ public:
         chunk_pair(offset + ai_write_mate_2, std::move(encoded_reads_2)));
     }
 
+    m_stats.return_sink(std::move(stats));
+
     return chunks;
   }
-
-private:
-  const userconfig& m_config;
-  const size_t m_nth;
 };
 
 int
@@ -140,6 +145,7 @@ demultiplex_sequences_se(const userconfig& config)
   std::cerr << "Demultiplexing single ended reads ..." << std::endl;
 
   scheduler sch;
+  std::vector<reads_processor*> processors;
   ar_statistics stats;
 
   try {
@@ -168,9 +174,9 @@ demultiplex_sequences_se(const userconfig& config)
       const size_t offset = nth * ai_analyses_offset;
       const std::string& sample = config.adapters.get_sample_name(nth);
 
-      sch.add_step(offset + ai_trim_se,
-                   "process_se_" + sample,
-                   new se_demultiplexed_reads_processor(config, nth));
+      processors.push_back(new se_demultiplexed_reads_processor(config, nth));
+      sch.add_step(
+        offset + ai_trim_se, "process_se_" + sample, processors.back());
 
       add_write_step(
         config,
@@ -187,7 +193,7 @@ demultiplex_sequences_se(const userconfig& config)
 
   if (!sch.run(config.max_threads)) {
     return 1;
-  } else if (!demux_write_report(config, stats)) {
+  } else if (!demux_write_report(config, processors, stats)) {
     return 1;
   }
 
@@ -200,6 +206,7 @@ demultiplex_sequences_pe(const userconfig& config)
   std::cerr << "Demultiplexing paired end reads ..." << std::endl;
 
   scheduler sch;
+  std::vector<reads_processor*> processors;
   ar_statistics stats;
 
   try {
@@ -249,9 +256,9 @@ demultiplex_sequences_pe(const userconfig& config)
       const size_t offset = nth * ai_analyses_offset;
       const std::string& sample = config.adapters.get_sample_name(nth);
 
-      sch.add_step(offset + ai_trim_pe,
-                   "process_pe_" + sample,
-                   new pe_demultiplexed_reads_processor(config, nth));
+      processors.push_back(new pe_demultiplexed_reads_processor(config, nth));
+      sch.add_step(
+        offset + ai_trim_pe, "process_pe_" + sample, processors.back());
 
       add_write_step(
         config,
@@ -277,7 +284,7 @@ demultiplex_sequences_pe(const userconfig& config)
 
   if (!sch.run(config.max_threads)) {
     return 1;
-  } else if (!demux_write_report(config, stats)) {
+  } else if (!demux_write_report(config, processors, stats)) {
     return 1;
   }
 
