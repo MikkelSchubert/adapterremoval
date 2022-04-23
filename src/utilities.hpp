515e98d764b693683561fe7c7c621334fa0acd0a
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2022-04-23 09:35:27 +0200

    no need for a custom implementation of make shared

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -37,9 +37,4 @@ make_unique(Args&&... args)
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
-template<typename T, typename... Args>
-std::shared_ptr<T>
-make_shared(Args&&... args)
-{
-  return std::shared_ptr<T>(new T(std::forward<Args>(args)...));
-}
+using std::make_shared;
