eec480b563da2faffde930527b992b24a2a99799
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2022-08-02 16:00:11 +0200

    add "[default: X]" field to --barcode-mm help text

--- src/userconfig.cpp
+++ src/userconfig.cpp
@@ -422,7 +422,8 @@ userconfig::userconfig(const std::string& name,
     argparser["--barcode-mm"] =
         new argparse::knob(&barcode_mm, "N",
             "Maximum number of mismatches allowed when counting mismatches in "
-            "both the mate 1 and the mate 2 barcode for paired reads.");
+            "both the mate 1 and the mate 2 barcode for paired reads "
+            "[default: %default].");
     argparser["--barcode-mm-r1"] =
         new argparse::knob(&barcode_mm_r1, "N",
             "Maximum number of mismatches allowed for the mate 1 barcode; "
