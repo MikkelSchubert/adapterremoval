caf014dc792fb0dae22a8de6a0a8f0adad416a22
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2023-06-13 13:18:05 +0200

    wrong option names used in help text

--- src/userconfig.cpp
+++ src/userconfig.cpp
@@ -764,11 +764,11 @@ userconfig::userconfig()
     .bind_double(&trim_mott_rate)
     .with_default(0.05);
   argparser.add("--trim-windows", "X")
-    .help("Specifies the size of the window used for --trimming-strategy "
-          "'window': If >= 1, this value will be used as the window size; if "
-          "the value is < 1, window size is the read lenght times this value. "
-          "If the resulting window size is 0 or larger than the read length, "
-          "the read length is used as the window size")
+    .help("Specifies the size of the window used for --trim-strategy 'window': "
+          "If >= 1, this value will be used as the window size; if the value "
+          "is < 1, window size is the read lenght times this value. If the "
+          "resulting window size is 0 or larger than the read length, the read "
+          "length is used as the window size")
     .deprecated_alias("--trimwindows")
     .conflicts_with("--trim-mott-rate")
     .conflicts_with("--trim-qualities")
@@ -776,7 +776,7 @@ userconfig::userconfig()
     .with_default(0.1);
   argparser.add("--trim-min-quality", "N")
     .help("Inclusive minimum quality used when trimming low-quality bases with "
-          "--trimming-strategy 'window' and 'per-base'")
+          "--trim-strategy 'window' and 'per-base'")
     .deprecated_alias("--minquality")
     .conflicts_with("--trim-mott-rate")
     .bind_uint(&trim_quality_score)
