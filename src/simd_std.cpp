6b5a2587d7e7ba2c8b6a9bec3d734c307864ccdc
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2024-02-28 22:47:07 +0100

    allow loop unrolling in non-SIMD alignments
    This results in a 4-5x increase in throughput when no SIMD acceleration is available

--- src/simd_std.cpp
+++ src/simd_std.cpp
@@ -23,33 +23,60 @@ namespace adapterremoval {
 
 namespace simd {
 
+namespace {
+
+//! Number of nucleotides to compare per (unrolled) loop
+const size_t LOOP_COUNT = 8;
+
+} // namespace
+
 bool
-compare_subsequences_std(size_t& n_mismatches,
-                         size_t& n_ambiguous,
+compare_subsequences_std(size_t& out_mismatches,
+                         size_t& out_ambiguous,
                          const char* seq_1,
                          const char* seq_2,
                          size_t length,
                          size_t max_penalty)
 {
-  size_t penalty = 2 * n_mismatches + n_ambiguous;
+  // Local accumulators are used since references prevent loop unrolling
+  size_t n_mismatches = out_mismatches;
+  size_t n_ambiguous = out_ambiguous;
+
+  while (length >= LOOP_COUNT) {
+    // A constant loop count enables loop-unrolling for increased throughput
+    for (size_t i = 0; i < LOOP_COUNT; ++i) {
+      const char nt_1 = *seq_1++;
+      const char nt_2 = *seq_2++;
+
+      if (nt_1 == 'N' || nt_2 == 'N') {
+        n_ambiguous++;
+      } else if (nt_1 != nt_2) {
+        n_mismatches++;
+      }
+    }
+
+    if (2 * n_mismatches + n_ambiguous > max_penalty) {
+      return false;
+    }
+
+    length -= LOOP_COUNT;
+  }
+
   for (; length; --length) {
     const char nt_1 = *seq_1++;
     const char nt_2 = *seq_2++;
 
     if (nt_1 == 'N' || nt_2 == 'N') {
       n_ambiguous++;
-      penalty++;
     } else if (nt_1 != nt_2) {
       n_mismatches++;
-      penalty += 2;
-    }
-
-    if (penalty > max_penalty) {
-      return false;
     }
   }
 
-  return true;
+  out_ambiguous = n_ambiguous;
+  out_mismatches = n_mismatches;
+
+  return 2 * n_mismatches + n_ambiguous <= max_penalty;
 }
 
 } // namespace simd
