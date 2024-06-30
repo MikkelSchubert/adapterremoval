aa856edfb0db26e18a917618f4414fb4d727ed84
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2024-06-30 23:22:27 +0200

    add missing pragma once

--- src/benchmarking.hpp
+++ src/benchmarking.hpp
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License     *
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 \*************************************************************************/
+#pragma once
+
 #include "commontypes.hpp" // for string, string_vec
 #include <chrono>          // for high_resolution_clock
 #include <vector>          // for vector
