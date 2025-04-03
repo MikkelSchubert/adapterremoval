f5910ee0fe9340adf3bfab1929574206aecfc020
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2025-04-03 19:16:18 +0200

    fix formatting

--- src/commontypes.hpp
+++ src/commontypes.hpp
@@ -3,8 +3,8 @@
 // SPDX-FileCopyrightText: 2014 Mikkel Schubert <mikkelsch@gmail.com>
 #pragma once
 
-#include <string_view> // for string_view
 #include <string>      // for string
+#include <string_view> // for string_view
 #include <vector>      // for vector
 
 namespace adapterremoval {
