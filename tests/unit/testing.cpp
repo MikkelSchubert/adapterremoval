5f6e9b42c011ede6b7d61341dfcea1e354071938
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2025-04-16 00:23:49 +0200

    replace default catch2 string rendering (#125)
    Detect if strings contain unprintable characters or whitespace that
    cannot easily be distinguished, and escape everything if that is the case

--- tests/unit/testing.cpp
+++ tests/unit/testing.cpp
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2025 Mikkel Schubert <mikkelsch@gmail.com>
 #include "testing.hpp"  // declarations
 #include "strutils.hpp" // for log_escape
+#include <cctype>       // for isprint
 
 namespace {
 
@@ -22,6 +23,23 @@ Catch::StringMaker<std::invalid_argument, void>::convert(
   const std::invalid_argument& value)
 {
   return format_exception("invalid_argument", value);
+}
+
+std::string
+StringMaker<std::string>::convert(const std::string& str)
+{
+  if (getCurrentContext().getConfig()->showInvisibles()) {
+    return adapterremoval::log_escape(str);
+  }
+
+  for (const auto c : str) {
+    // Escape any special characters that cannot be distinguished at a glance
+    if (!std::isprint(c) || (std::isspace(c) && (c != ' ' && c != '\n'))) {
+      return adapterremoval::log_escape(str);
+    }
+  }
+
+  return '"' + str + '"';
 }
 
 } // namespace Catch
