ccfb3fe11d83fe7e946a40ded4563d3ae5c597ae
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2025-09-12 10:43:34 +0200

    20% higher throughput for PE trimming using NEON (#152)
    Process blocks in batches using NEON SIMD instructions.
    
    This yields a roughly 2x increase in alignment speed for PE alignments and a small (up to 5%) decrease in alignment speed for shorter SE alignments (tested on 1M reads/read pairs):
    
    ```
         Benchmark |       Min |      Mean |       Max | SD (%) | Loops | Outliers
    PE  90 bp, new |   705,471 |   706,840 |   709,209 |   0.19 |    10 |        0
    PE  90 bp, old | 1,353,461 | 1,355,887 | 1,358,186 |   0.14 |    10 |        0
    
    PE 151 bp, new | 1,274,855 | 1,275,260 | 1,276,002 |   0.04 |    10 |        0
    PE 151 bp, old | 2,699,288 | 2,703,584 | 2,709,823 |   0.16 |    10 |        0
    
    PE 251 bp, new | 3,044,884 | 3,062,679 | 3,078,010 |   0.37 |    10 |        0
    PE 251 bp, old | 6,193,364 | 6,201,703 | 6,226,089 |   0.18 |    10 |        2
    
    SE  90 bp, new |   315,076 |   316,141 |   319,009 |   0.39 |    16 |        1
    SE  90 bp, old |   317,847 |   318,442 |   319,223 |   0.14 |    16 |        2
    
    SE 151 bp, new |   493,804 |   495,057 |   497,465 |   0.24 |    11 |        1
    SE 151 bp, old |   483,017 |   483,302 |   483,591 |   0.04 |    11 |        0
    
    SE 251 bp, new |   792,004 |   793,074 |   794,197 |   0.10 |    10 |        1
    SE 251 bp, old |   756,888 |   758,570 |   760,779 |   0.21 |    10 |        1
    ```
    
    In real terms, this yields a ~20% increase in end-to-end throughput with merging and compressed output for PE reads, and no measurable change for SE reads:
    
    ```
    K reads/second, for 1M reads/read pairs:
    
                old   new
    PE  90 bp   614   705
    PE 151 bp   443   529
    PE 251 bp   221   268
    
    SE  90 bp   680   680
    SE 151 bp   529   529
    SE 251 bp   289   289
    ```

--- src/simd_neon.cpp
+++ src/simd_neon.cpp
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2022 Mikkel Schubert <mikkelsch@gmail.com>
-// #include "simd.hpp"    // declarations
+#include "simd.hpp"   // declarations
 #include <algorithm>  // for min
 #include <arm_neon.h> // for vdupq_n_u8, vld1q_u8, vorrq_u8, ...
 #include <cstddef>    // for size_t
 
-namespace adapterremoval {
-
-namespace simd {
+namespace adapterremoval::simd {
 
 namespace {
 
-/** Counts the number of masked bytes **/
-inline auto
-count_masked_neon(uint8x16_t value)
-{
-  // Extract one bit per uint8_t and sum across vector
-  return vaddvq_u8(vandq_u8(vdupq_n_u8(0x01), value));
-}
+//! The number of bytes in a NEON uint8x16_t
+const size_t NEON_BLOCK_SIZE = 16;
+//! The number of uint8x16_t to process between before summing up stats
+const size_t NEON_BATCH_SIZE = NEON_BLOCK_SIZE * 8;
 
 } // namespace
 
@@ -26,43 +21,45 @@ compare_subsequences_neon(size_t& n_mismatches,
                           size_t& n_ambiguous,
                           const char* seq_1,
                           const char* seq_2,
-                          size_t length,
+                          const size_t length,
                           size_t max_penalty)
 {
   //! Mask of all Ns
   const auto n_mask = vdupq_n_u8('N');
 
   // The sequence is assumed to be padded with 15 'N's not included in `length`.
-  while (length) {
-    auto s1 = vld1q_u8(reinterpret_cast<const uint8_t*>(seq_1));
-    auto s2 = vld1q_u8(reinterpret_cast<const uint8_t*>(seq_2));
+  auto i_length = static_cast<int64_t>(length);
+  while (i_length > 0) {
+    auto a_matches = vdupq_n_u8(0);
+    auto a_ambiguous = vdupq_n_u8(0);
 
-    // Sets 0xFF for every byte where one or both nts is N
-    const auto ns_mask = vorrq_u8(vceqq_u8(s1, n_mask), vceqq_u8(s2, n_mask));
+    size_t chunk = 0;
+    for (; chunk < NEON_BATCH_SIZE && i_length > 0; chunk += NEON_BLOCK_SIZE) {
+      auto s1 = vld1q_u8(reinterpret_cast<const uint8_t*>(seq_1));
+      auto s2 = vld1q_u8(reinterpret_cast<const uint8_t*>(seq_2));
 
-    // Sets 0xFF for every byte where bytes are equal or N
-    const auto eq_mask = vorrq_u8(vceqq_u8(s1, s2), ns_mask);
+      // Sets 0xFF for every byte where one or both nts is N
+      const auto ns_mask = vorrq_u8(vceqq_u8(s1, n_mask), vceqq_u8(s2, n_mask));
 
-    n_mismatches += 16 - count_masked_neon(eq_mask);
+      // Sets 0xFF for every byte where bytes are equal or N
+      const auto eq_mask = vorrq_u8(vceqq_u8(s1, s2), ns_mask);
+
+      a_matches = vaddq_u8(a_matches, vshrq_n_u8(eq_mask, 7));
+      a_ambiguous = vaddq_u8(a_ambiguous, vshrq_n_u8(ns_mask, 7));
+
+      seq_1 += NEON_BLOCK_SIZE;
+      seq_2 += NEON_BLOCK_SIZE;
+      i_length -= NEON_BLOCK_SIZE;
+    }
+
+    n_mismatches += chunk - vaddvq_u8(a_matches);
+    n_ambiguous += vaddvq_u8(a_ambiguous) + std::min<int64_t>(0, i_length);
     if (2 * n_mismatches + n_ambiguous > max_penalty) {
       return false;
     }
-
-    // Fragment length without 'N' padding
-    size_t unpadded_length = std::min<size_t>(16, length);
-
-    // Early termination is almost always due to mismatches, so updating the
-    // number of Ns after the above check saves time in the common case.
-    n_ambiguous += count_masked_neon(ns_mask) - (16 - unpadded_length);
-
-    seq_1 += 16;
-    seq_2 += 16;
-    length -= unpadded_length;
   }
 
   return true;
 }
 
-} // namespace simd
-
-} // namespace adapterremoval
+} // namespace adapterremoval::simd
