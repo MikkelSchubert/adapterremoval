d15542b851fdab8f579741e382e1098a1901fcac
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-01-18 18:04:15 +0100

    remove workaround (issue fixed in last pull)

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -17,12 +17,6 @@ done
 mv -v "${PREFIX}/share/adapterremoval3/examples" "${TARGET}/examples"
 mv -v "${PREFIX}/share/adapterremoval3/docs/html" "${TARGET}/docs"
 mv -v "${PREFIX}/share/man/man1/adapterremoval3.1" "${TARGET}/docs/"
-
-
-# WORKAROUND: libdeflate cannot be linked statically
-if test -e "/ucrt64/bin/libdeflate.dll"; then
-    cp "/ucrt64/bin/libdeflate.dll" "${TARGET}"
-    cp "/ucrt64/share/licenses/libdeflate/COPYING" "${TARGET}/docs/libdeflate.COPYING"
-fi
+cp -v LICENSE "${TARGET}/LICENSE.txt"
 
 tar cvzf "${TARGET}.tar.gz" "${TARGET}"
