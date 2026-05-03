1c33add55bf8e6850a0a7ec79b5294d1579dae08
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-05-03 20:26:49 +0200

    fix static/release builds (#312)
    - fix use of bool values for features
    - fix hardcoded use of ccache on windows
    - fix potential use of non-static libraries on old distros
    - remove unused feature option

--- scripts/setup-windows-latest.sh
+++ scripts/setup-windows-latest.sh
@@ -11,9 +11,16 @@ wget https://github.com/ebiggers/libdeflate/releases/download/v1.25/libdeflate-1
 tar xvzf libdeflate-1.25.tar.gz
 
 cd libdeflate-1.25
-cmake -B build \
-    -DCMAKE_INSTALL_PREFIX=/ucrt64 \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+
+if command -v ccache; then
+    cmake -B build \
+        -DCMAKE_INSTALL_PREFIX=/ucrt64 \
+        -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+        -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+else
+    cmake -B build \
+        -DCMAKE_INSTALL_PREFIX=/ucrt64
+fi
+
 cmake --build build
 cmake --install build
