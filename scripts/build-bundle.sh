7051d4b090ec4a86432f3def2e1b848d636fab8d
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-08-20 21:56:43 +0200

    improve release workflow
    - zip Windows build instead of using tarball
    - explicitly create source tarball

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -4,6 +4,7 @@ trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 PREFIX=$1
 TARGET=$2
+FORMAT=$3
 
 mkdir -p "${TARGET}"
 for exe in adapterremoval3.exe adapterremoval3; do
@@ -19,4 +20,11 @@ mv -v "${PREFIX}/share/adapterremoval3/docs/html" "${TARGET}/docs"
 mv -v "${PREFIX}/share/man/man1/adapterremoval3.1" "${TARGET}/docs/"
 cp -v LICENSE "${TARGET}/LICENSE.txt"
 
-tar cvzf "${TARGET}.tar.gz" "${TARGET}"
+if test "${FORMAT}" = "tar.gz"; then
+    tar cvzf "${TARGET}.tar.gz" "${TARGET}"
+elif test "${FORMAT}" = "zip"; then
+    zip -r9 "${TARGET}.zip" "${TARGET}"
+else
+    echo "Unsupported format: '${FORMAT}'"
+    exit 1
+fi
